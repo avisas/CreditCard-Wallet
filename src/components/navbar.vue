9b850ec0ec8585ca088669dd6e596ce0d1e4741c
avisas <alejandra.visa91@gmail.com>
2019-12-03 12:17:22 -0500

    navbar upd

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,12 +1,24 @@
 <template>
     <nav>
-        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="navbar navbar-expand-lg navbar-light bg-dark">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    <img>
+                    <div class="logo-image">
+                    <img src="../assets/ivisa-logo-navbar.png" class="img-fluid">
+                    </div>
                 </a>
                 <router-link to="/" class="brand-logo">My Cards</router-link>
             </div>
         </div>
     </nav>
 </template>
+
+<style scoped>
+.logo-image{
+    width: 25%;
+    height: 25%;
+    border-radius: 50%;
+    overflow: hidden;
+    margin-top: -6px;
+}
+</style>
