8354c43320a9e850a17bc33d125ae7d39369eede
avisas <alejandra.visa91@gmail.com>
2019-12-05 18:54:17 -0500

    css modified

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 
 export default {
   name: 'App',
@@ -22,7 +22,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="wallet">
-    <div class="card m-4">
-    <div class="card-header">My Cards</div>
-      <div class="card-body">
-        <ul v-for="card in cards" :key="card.id">
-          <li>
-        {{card}}
-          <b-button v-b-modal="`modal-rm-${card.id}`">Remove</b-button>
-          <b-modal :id="`modal-rm-${card.id}`" ok-title="Yes" @ok="removeCard">
-            <img src="../assets/remove-card-icon.svg" style="width: 25%">
+  <div class="main-cards">
+    <div class="card-hd">My Cards</div>
+      <ul class="cards-container">
+        <li class="item-card" v-for="card in cards" :key="card.id">
+        <!-- {{card}} -->
+        <img class="icon-check" src="../assets/Ok-green-ico.svg">
+        <div class="card-container">
+          <div class="info-card-container">
+              <img class="icon-type-card" src="../assets/visa-card-logo.svg">
+              <div class="info-card">
+                <p >{{card.type}} {{card.cardNumber}}</p>
+                <p >Exp. Date: {{card.expMonth}}/{{card.expYear}}</p>
+              </div> 
+          </div>
+          <div class="btn-rm-sd">
+                  <button v-b-modal="`modal-rm-${card.id}`">Remove</button>
+                  <button v-b-modal="`modal-sd-${card.id}`">Set Default</button>
+          </div> 
+        </div>
+        <b-modal  :id="`modal-rm-${card.id}`" ok-title="Yes" @ok="removeCard">
+          <img src="../assets/remove-card-icon.svg" style="width: 25%">
             <h4>Remove card</h4>
             <p class="my-4">
             Are you sure you want to remove from wallet?
             </p>
           </b-modal>
 
-          <b-button v-b-modal="`modal-sd-${card.id}`">Set Default</b-button>
           <b-modal :id="`modal-sd-${card.id}`" ok-title="Yes" @ok="setDefault">
             <img src="../assets/default-card-icon.svg" style="width: 25%">
             <h4>Change default card</h4>
@@ -25,38 +36,49 @@
             </p>
           </b-modal>
             </li>
-          </ul>
-        </div>
+        </ul>
     </div>
-    <div class="card m-4">
-    <div class="card-header">+ Add New Card</div>
-      <div v-if="true" class="card-body">
-        <img src="../assets/ok-green-ico.svg" style="width: 5%">
-        <label class="p-accept-cards"><strong>Accept All Major Debit/ Credit Cards</strong></label>
-        <img src="../assets/Payment-methods.jpg">
-        <label for="cardName">Name on Card</label>
-        <input v-model.lazy.trim="cardName" required>
-        <label for="cardNumber">Credit/ Debit Card Number</label>
-        <input v-model.lazy.number="cardNumber" type="number" required>
-        <label for="expMonth">Exp. Month</label>
+    <div class="main-cards main-cards-more">
+    <div class="card-hd">+ Add New Card</div>
+      <form v-if="true" class="card-body">
+        <div class="major-cards">
+          <div class="info-accept-cards">
+          <img class="icon-check-show" src="../assets/ok-green-ico.svg">
+          <label class="p-accept-cards"><strong>We Accept All Major Debit/ Credit Cards</strong></label>
+        </div>
+          <img class="img-all-cards" src="../assets/Payment-methods.jpg">
+        </div>
+
+       <div class="input-new-cards">
+         <div>
+           <label for="cardName">Name on Card</label>
+           <input v-model.lazy.trim="cardName" required>
+           </div>
+        <div><label for="cardNumber">Credit/ Debit Card Number</label>
+        <input v-model.lazy.number="cardNumber" type="number" required></div>
+        <div><label for="expMonth">Exp. Month</label>
         <select v-model.lazy.number="expMonth" required>
           <option v-for="month in months" :value="month.val" :key="month.val">
             {{month.desc}}
           </option>
-        </select>
-        <label for="expYear">Exp. Year</label>
+        </select></div>
+        <div><label for="expYear">Exp. Year</label>
         <select v-model.lazy.number="expYear" required>
           <option v-for="year in years" :value="year.val" :key="year.val">
             {{year.desc}}
           </option>
-        </select>
-        <label for="securityCode">Security Code</label>
-        <input v-model.lazy.number="secCode" type="number" required>
-        <div class="col-m-2">
-          <img src="../assets/Sectigo-trust-logo.png" alt="sectigo-logo" style="width: 10%">
-          <button @click="addCard" class="add-card-btn" style="width: 80%">Add Card</button>
+        </select></div>
+        <div><label for="securityCode">Security Code</label>
+        <input v-model.lazy.number="secCode" type="number" required></div>
+        
+
+      <div class="add-card-container">
+          <img src="../assets/Sectigo-trust-logo.png" alt="sectigo-logo">
+          <button @click="addCard" class="add-card-btn">Add Card</button>
         </div>
-      </div>
+
+       </div>        
+      </form>
     </div>
   </div>
 </template>
@@ -205,6 +227,15 @@ export default {
       }
       return 'UNKNOWN';
     },
+    typeCard() {
+      const typeCard = card.type;
+      console.log(card.type);
+      
+      // return typeCard;
+      // if(){
+
+      // }
+    }
   },
 };
 </script>
@@ -219,20 +250,252 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+button, a, input, select{
+    outline: none;
+  }
+
+  /* Modales */
+.modals {
+   background-color: #52d03e !important;
 }
-a {
-  color: #42b983;
+  /* Fin Modales */
+
+.main-cards{
+max-width: 900px;
+    margin: auto;
+    position: relative;
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    border-radius: 0.25rem;
+    box-shadow: 0px 0px 13px -5px #606060;
+    }
+.card-hd{
+      text-align: left;
+    border-bottom: 1px solid #e6e6e6;
+    padding: 20px;
+    color: #426aa0;
+    font-weight: 500;
+    font-size: 18px;
 }
-.add-card-btn{
-  border: none;
-  background-color: #52d03e;
-  color: white;
-  border-radius: 8px;
+.cards-container{
+  width: 100%;
+    padding: 15px 12px 0px;
 }
+.icon-check{
+  width: 14px;
+  margin-right: 18px;
+}
+.item-card{
+  display: flex;
+    align-items: center;
+    background-color: #fafafa;
+    margin-bottom: 10px;
+    padding: 20px;
+}
+.info-card-container{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+    min-width: 200px;
+    margin: 10px 0;
+}
+
+.card-container{
+  width: calc(100% - 32px);
+}
+.icon-type-card{
+  width: 95px;
+    margin-right: 20px;
+    margin: 0px 14px 10px 0px;
+}
+.info-card{
+  color: #6c757d;
+  text-align: left;
+
+}
+.info-card p{
+  margin: 0
+
+}
+.info-card p:nth-child(1){
+  font-size: 12px;
+
+}
+.info-card p:nth-child(2){
+  font-size: 10px;
+
+}
+.btn-rm-sd{
+min-width: 200px;
+    display: flex;
+    justify-content: space-between;
+}
+.btn-rm-sd button{
+  background-color: initial;
+    color: #c2c2c2;
+    border: initial;
+    font-size: 12px;
+    text-decoration: underline;
+    font-weight: bold;
+}
+.main-cards-more{
+      margin-top: 16px;
+    margin-bottom: 40px;
+}
+.major-cards{
+      display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-bottom: 20px;
+}
+.input-new-cards{
+  text-align: center;
+}
+.input-new-cards label{
+  margin: 0;
+    width: 90%;
+    text-align: left;
+    font-size: 15px;
+    margin-bottom: 0px;
+}
+
+.input-new-cards input,
+.input-new-cards select{
+     width: 90%;
+    border-radius: 5px;
+    border: 1px solid #9ac193;
+    height: 30px;
+    margin-bottom: 8px;
+}
+
+.info-accept-cards{
+  display: flex;
+    width: 200px;
+    font-size: 15px;
+    line-height: 18px;
+}
+
+.icon-check-show{
+  width: 38px;
+    height: 38px;
+    margin-right: 10px;
+}
+
 .p-accept-cards{
   color: #52d03e;
 }
+.img-all-cards{
+  width: 285px;
+}
+.add-card-container{
+      display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+        
+}
+.add-card-container img{
+  width: 100px;
+    margin: 5px 0 5px;
+}
+.add-card-btn{
+ border: none;
+    background-color: #52d03e;
+    color: white;
+    border-radius: 8px;
+    width: 90%;
+    padding: 6px 10px;
+    flex-grow: 1;
+    height: 37px;
+}
+
+@media (min-width: 600px) {
+  .card-container{
+    width: calc(100% - 32px);
+    display: flex;
+    justify-content: space-between;
+  }
+  .icon-type-card{
+    margin: 0px 16px 0px 0px;
+}
+.info-card-container{
+
+}
+.btn-rm-sd{
+  justify-content: flex-end;
+  }
+  .cards-container {
+    width: 100%;
+    padding: 15px 20px 0px;
+}
+
+.major-cards{
+  justify-content: left;
+}
+.input-new-cards{
+ display: flex;
+    flex-wrap: wrap;
+    text-align: initial;
+}
+.input-new-cards div:not(.add-card-container){
+  
+}
+.input-new-cards div:not(.add-card-container):nth-child(1),
+.input-new-cards div:not(.add-card-container):nth-child(2){
+    width: 27%;
+    min-width: 200px;
+    flex-grow: 1;
+}
+.input-new-cards div:not(.add-card-container):nth-child(3),
+.input-new-cards div:not(.add-card-container):nth-child(4),
+.input-new-cards div:not(.add-card-container):nth-child(5){
+  width: 14%;
+    min-width: 110px;
+}
+
+.input-new-cards input,
+.input-new-cards select{
+     width: 90%;
+    border-radius: 5px;
+    border: 1px solid #9ac193;
+    height: 30px;
+    margin-bottom: 8px;
+}
+
+.info-accept-cards{
+  display: flex;
+    width: 200px;
+    font-size: 15px;
+    line-height: 18px;
+}
+
+.icon-check-show{
+  width: 38px;
+    height: 38px;
+    margin-right: 10px;
+}
+
+.p-accept-cards{
+  color: #52d03e;
+}
+.img-all-cards{
+  width: 285px;
+}
+.add-card-container{
+   width: 100%;
+      display: flex;
+    margin-top: 10px;
+}
+.add-card-container img{
+  width: 100px;
+    margin: 5px 0 5px;
+}
+.add-card-btn{
+     width: 76%;
+    padding: 0px 12px;
+    margin: 0 10px;
+}
+}
+
+
 </style>
